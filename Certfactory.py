92d53812ff908fe31b773e03473a596621a25a09
antarasargam <antarasargam@gmail.com>
2017-11-11 00:27:35 -0500

    Update Certfactory.py

--- Certfactory.py
+++ Certfactory.py
@@ -1,20 +1,29 @@
 def getPrivateKeyForAddr():
-    with open("/root/keys/client/sagar.key") as fp:
+    with open("/root/antaraprivateclient") as fp:
         private_key_user = fp.read()
     return private_key_user
 
 def getCertsForAddr():
-    with open("/root/keys/paas_signed.cert") as fp:
+    with open("/root/Downloads/paas_signed.cert") as fp:
         pub_key_IACA = fp.read()
     return pub_key_IACA
 
 def getIDCertsForAddr():
-    with open("/root/keys/client/client_cert") as fp:
+    with open("/root/Downloads/client_cert") as fp:
         id_key_IACA = fp.read()
     return id_key_IACA
 
 def getRootCertsForAddr():
-    with open("/root/keys/root.crt") as fp:
+    with open("/root/Downloads/root.crt") as fp:
         root_key_IACA = fp.read()
     return root_key_IACA
 
+def getPrivateKeyForAddrServer():
+    with open("/root/antaraprivate") as fp:
+        private_key_user = fp.read()
+    return private_key_user
+
+def getIDCertsForAddrServer():
+    with open("/root/Downloads/server_cert") as fp:
+        id_key_IACA = fp.read()
+    return id_key_IACA
